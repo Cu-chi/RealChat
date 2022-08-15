1258899968598c761d0e34467ad42c3f9fa6535c
Cu-chi <jusaumsp@gmail.com>
2022-08-16 00:13:57 +0200

    First Commit
    setting up project

---
+++ RealChat/MainForm.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace RealChat
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(41)))), ((int)(((byte)(61)))));
+            this.ClientSize = new System.Drawing.Size(879, 483);
+            this.MinimumSize = new System.Drawing.Size(540, 400);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "RealChat";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ docs/project_guidelines.md
@@ -0,0 +1,5 @@
+# Colors
+Primary color: ![#13293D](https://via.placeholder.com/16/13293D/13293D.png) #13293D   
+Secondary color: ![#E8F1F2](https://via.placeholder.com/16/E8F1F2/E8F1F2.png) #E8F1F2  
+Red color: ![#F24333](https://via.placeholder.com/16/F24333/F24333.png) #F24333  
+Orange color: ![#F4AC32](https://via.placeholder.com/16/F4AC32/F4AC32.png) #F4AC32  
